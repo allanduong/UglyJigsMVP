53e1f752ef9b65430015bbaf1a8ed2ebdfb41276
exknown <abduong206@yahoo.com>
2020-11-08 03:04:30 -0800

    edit contact page php

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -25,8 +25,8 @@ if (empty($_POST["terms"])) {
     $terms = $_POST["terms"];
 }
 
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Aria landing page";
+$EmailTo = "Uglyjigs@gmail.com";
+$Subject = "You have received a new message from UglyJigs landing page!";
 
 // prepare email body text
 $Body = "";
